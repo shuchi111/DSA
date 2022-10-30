48965eb93f37d0cf895f58ab77b1dd17469e9f75
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2022-10-31 00:55:52 +0530

    Update satisfy the equation.cc

--- satisfy the equation.cc
+++ satisfy the equation.cc
@@ -1,3 +1,10 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
 class Solution {
   public:
     vector<int> satisfyEqn(int A[], int N) {
@@ -38,3 +45,30 @@ class Solution {
        
     }
 };
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int A[N];
+        
+        for(int i=0; i<N; i++)
+            cin>>A[i];
+
+        Solution ob;
+        vector<int> ptr = ob.satisfyEqn(A,N);
+        
+        cout<<ptr[0]<<" "<<ptr[1]<<" "<<ptr[2]<<" "<<ptr[3]<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+                
+
+   
+      
