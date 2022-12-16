77486fb858b44bc1be0ff30998a7df9aedb5c73d
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2022-12-17 03:31:06 +0530

    Break a number

---
+++ Break a Number.cc
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{   
+public:
+    int waysToBreakNumber(int N){
+        // code here 
+          const unsigned int M = 1000000007;
+
+        
+
+        long long Y=(N+2)%M;
+
+        long long Z=(N+1)%M;
+
+        long long ans=((Y*Z)/2)%M;
+
+        return ans;
+
+    
+
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.waysToBreakNumber(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
